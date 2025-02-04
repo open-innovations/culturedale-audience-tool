f84803a160bf84272a78b744b104786623cf217c
slowe <299787+slowe@users.noreply.github.com>
2025-02-04 19:52:19 +0000

    Switch to OI Lume Viz hex cartogram

--- _config.ts
+++ _config.ts
@@ -4,12 +4,18 @@ import lume from "lume/mod.ts";
 import esbuild from "lume/plugins/esbuild.ts";
 import basePath from "lume/plugins/base_path.ts";
 
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.16.6/mod.ts";
+import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
+
 const site = lume({
   src: "./src",
   location: new URL(
     "https://open-innovations.github.io/culturedale-audience-tool/",
   ),
 });
+
+import oiVizConfig from "./oi-viz-config.ts";
+site.use(oiViz(oiVizConfig));
 
 site.use(esbuild({
   extensions: [".ts"],
@@ -44,5 +50,10 @@ site.use(basePath());
 
 site.copy([".css"]);
 site.copy("assets/vendor");
+
+
+// Register an HTML processor
+// https://lume.land/docs/core/processors/
+site.process([".html"], (pages) => pages.forEach(autoDependency));
 
 export default site;
--- src/assets/js/_lib/hex-map.ts
+++ src/assets/js/_lib/hex-map.ts
@@ -1,40 +1,59 @@
-import { HexMap } from "https://cdn.jsdelivr.net/gh/dringtech/web-building-blocks@main/visualisation/hex-map.ts";
-
 import { CAT_RESULT } from "./handlers-result.ts";
 import { colourScale } from "./colour.ts";
 
-customElements.define("hex-map", HexMap);
+// Return the most contrasty colour
+// https://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
+// https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html#contrast-ratiodef
+function d2h(d){ return ((d < 16) ? "0" : "") + d.toString(16); }
+function h2d(h){ return parseInt(h, 16); }
+function toLin(v){ v /= 255; if (v <= 0.03928){ return v/12.92; }else{ return Math.pow((v+0.055)/1.055,2.4); }}
+function rLum(rgb){ return 0.2126 * toLin(rgb[0]) + 0.7152 * toLin(rgb[1]) + 0.0722 * toLin(rgb[2]); }
+function contrastRatio(a, b){ let L1 = rLum(a); let L2 = rLum(b); if(L1 < L2){ let temp = L2; L2 = L1; L1 = temp; } return (L1 + 0.05) / (L2 + 0.05); }
+function contrastColour(c){ let rgb = [h2d(c.substring(1,3)),h2d(c.substring(3,5)),h2d(c.substring(5,7))]; return (contrastRatio(rgb,[0, 0, 0]) > contrastRatio(rgb,[255, 255, 255]) ? "black" : "white"); }
 
 export function initialiseHexMap() {
-  const hexMap = document.querySelector<HexMap>("hex-map");
-  if (!hexMap) return;
 
-  // Define label function
-  hexMap.labelSpec = (c) => c.sn as string;
+	// Get the hexes and build a lookup to access their properties quickly
+	var hexes = document.querySelectorAll('.oi-map-map[data-type=hex-map] .hex');
+	var lookup = {},id,val,colour;
+	for(let i = 0; i < hexes.length; i++){
+		id = hexes[i].getAttribute('data-id');
+		lookup[id] = {
+			'g':hexes[i],
+			'path':hexes[i].querySelector('path'),
+			'title':hexes[i].querySelector('path title'),
+			'name':hexes[i].querySelector('path title').innerHTML,
+			'label':hexes[i].querySelector('text')
+		};
+		lookup[id].path.setAttribute('fill','#dfdfdf');
+	}
 
-  // Setup title render function
-  hexMap.titleSpec = (c) => `${c.n}: ${c.count} attendees`;
 
-  // Setup style render function
-  hexMap.styleSpec = (c) => ({
-    fill: colourScale(c.value).hex(),
-  });
+	addEventListener(CAT_RESULT, function (evt: CustomEvent) {
+		// Only interested in wards
+		const data = Object.entries<number>(evt.detail)
+		  .filter((x) => x[0].startsWith("E05"));
 
-  addEventListener(CAT_RESULT, function (evt: CustomEvent) {
-    // Only interested in wards
-    const data = Object.entries<number>(evt.detail)
-      .filter((x) => x[0].startsWith("E05"));
+		// Find the largest value
+		const max = Math.max(...data.map((x) => x[1]));
 
-    // Find the largest value
-    const max = Math.max(...data.map((x) => x[1]));
+		// Reset values
+		for(var id in lookup) lookup[id].value = 0;
 
-    // Update the hex map
-    hexMap.data = data.reduce((a, [k, v]) => ({
-      ...a,
-      [k]: {
-        count: v,
-        value: v / max,
-      },
-    }), {});
-  });
+		// Add data values to lookup
+		for(var i = 0; i < data.length; i++){
+			id = data[i][0];
+			val = data[i][1];
+			if(lookup[id]) lookup[id].value = val;
+		}
+
+		// Loop over all hexagons and set them
+		for(var id in lookup){
+			val = lookup[id].value;
+			colour = colourScale(val/max).hex();
+			lookup[id].path.setAttribute('fill',colour);
+			lookup[id].label.setAttribute('fill',contrastColour(colour));
+			lookup[id].title.innerHTML = lookup[id].name+'<br />Total: <strong>'+val.toLocaleString()+'</strong>';
+		}
+	});
 }
